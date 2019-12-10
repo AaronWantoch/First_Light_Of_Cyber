574533b8f5e59d83685c6a82733608d0b2321a56
credus <aaron.wantoch@gmail.com>
2019-12-10 17:44:10 +0100

    Initial Commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float chaseRange;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToPlayer = Mathf.Infinity;
+    bool isProvoked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(player.position, transform.position);
+
+        if(isProvoked)
+        {
+            Engage();
+        }
+        else if (distanceToPlayer <= chaseRange)
+        {
+            isProvoked = true;
+
+            Chase();
+        }
+    }
+
+    private void Engage()
+    {
+        if (distanceToPlayer <= navMeshAgent.stoppingDistance)
+        {
+            Fight();
+        }
+        else
+        {
+            Chase();
+        }
+    }
+
+    private void Fight()
+    {
+        print("Fighting");
+    }
+
+    private void Chase()
+    {
+        navMeshAgent.SetDestination(player.position);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the chase radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
