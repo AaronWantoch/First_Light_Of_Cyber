28350d3b8474a77dadb8f261fbc1b732b7ffc555
credus <aaron.wantoch@gmail.com>
2019-12-10 23:24:05 +0100

    Wepons and shooting

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float health;
+
+    public void DecreaseHealth(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Transform FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if(!Physics.Raycast(FPCamera.position, FPCamera.forward, out hit, range))
+        {
+            return;
+        }
+
+        EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
+        if(!enemyHealth)
+        {
+            return;
+        }
+        enemyHealth.DecreaseHealth(damage);
+    }
+}
