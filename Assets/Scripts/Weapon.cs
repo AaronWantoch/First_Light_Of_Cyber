0acb2ce88c1dc4de75b39c7ab92a1907899387d3
credus <aaron.wantoch@gmail.com>
2019-12-14 16:19:44 +0100

    Updated Guns

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -6,6 +7,8 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Transform FPCamera;
+    [SerializeField] ParticleSystem muzzleFlashFX;
+    [SerializeField] GameObject hitFX;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 5f;
 
@@ -27,17 +30,60 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        EmittMuzzleFlashParticles();
+        ProcessRaycast();
+    }
+
+    private void EmittMuzzleFlashParticles()
+    {
+        muzzleFlashFX.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(!Physics.Raycast(FPCamera.position, FPCamera.forward, out hit, range))
+
+        if (!Physics.Raycast(FPCamera.position, FPCamera.forward, out hit, range))
         {
             return;
         }
 
+        GenerateHitParticles(hit);
+
         EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
-        if(!enemyHealth)
+        if (!enemyHealth)
         {
             return;
         }
         enemyHealth.DecreaseHealth(damage);
+
+
+    }
+
+    private void GenerateHitParticles(RaycastHit hit)
+    {
+        GameObject hitObject 
+            = Instantiate(hitFX, hit.point, Quaternion.LookRotation(hit.normal));
+
+        ParticleSystem[] particleSystems = hitFX.GetComponentsInChildren<ParticleSystem>();
+        float duration = Maximum(particleSystems);
+
+        Destroy(hitObject, duration);
+    }
+
+    private float Maximum(ParticleSystem[] particleSystems)
+    {
+        float maximumDuration = 0f;
+
+        foreach(ParticleSystem particle in particleSystems)
+        {
+            float duration = particle.duration;
+            if (duration > maximumDuration)
+            {
+                maximumDuration = duration;
+            }
+        }
+
+        return maximumDuration;
     }
 }
