3d32f3bfb6190e9d6da60b334f2c388b90465e6a
credus <aaron.wantoch@gmail.com>
2020-02-27 06:51:07 +0100

    idk what I have done

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float health = 100f;
 
+    [SerializeField] Slider healthBar;
+
+    float maxHealth;
+
+    private void Start()
+    {
+        maxHealth = health;
+    }
+
     public void DecreaseHealth(float damage)
     {
         health -= damage;
+
+        healthBar.value = health / maxHealth;
+
         if (health <= 0)
         {
             HandleDeath();
@@ -25,6 +38,7 @@ public class PlayerHealth : MonoBehaviour
         Cursor.visible = true;
 
         FindObjectOfType<Weapon>().enabled = false;
-        FindObjectOfType<WeaponZoom>().enabled = false;
+        if(FindObjectOfType<WeaponZoom>())
+            FindObjectOfType<WeaponZoom>().enabled = false;
     }
 }
